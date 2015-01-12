33cf6c8abe22cbcc26124b454a680556c2316d5d
Artem Gorohov <gorohov.art@gmail.com>
2015-01-12 16:44:37 +0300

    fix

--- Library/Library1.fs
+++ Library/Library1.fs
@@ -1,18 +1,11 @@
 ﻿namespace Library
 open System.Drawing
 module Geometry = 
-//    type Point(x0, y0) =
-//        let mutable x : int = x0
-//        let mutable y : int = y0
-//        member this.X = x
-//        member this.Y = y
-
-    let segmentLength (A : Point) (B : Point) = 
-        
+    let squaredSegmentLength (A : Point) (B : Point) =        
         (A.X - B.X) * (A.X - B.X) + (A.Y - B.Y) * (A.Y - B.Y)
 
     let Checker A B C =
         let check A B C = 
-            (segmentLength A B = segmentLength B C + segmentLength A C)
+            (squaredSegmentLength A B = squaredSegmentLength B C + squaredSegmentLength A C)
         
         (check A B C) || (check A C B) || (check C B A)
